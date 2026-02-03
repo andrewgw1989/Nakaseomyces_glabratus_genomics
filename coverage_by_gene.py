cb405261538db732a0c9986bf17a668aa0fa6622
Andrew Gador-Whyte <a.gadorwhyte@unimelb.edu.au>
2026-02-03 11:59:20 +1100

    Update coverage_by_gene.py

--- coverage_by_gene.py
+++ coverage_by_gene.py
@@ -7,22 +7,34 @@ import sys
 import os
 import csv
 import subprocess
+import io
 
 bam_dir = sys.argv[1]
 
 with open("gene_coords.tsv",newline='') as f:
     genecoords = list(csv.reader(f, delimiter='\t'))
 
-with open("isolates.tsv","r") as input_file:
+with open("isolates.list","r") as input_file:
     for isolate in input_file:
         isolate = isolate.strip()
         bam_file=os.path.join(bam_dir, f"{isolate}.bam")
-        output_path = f"{isolate}.output.txt"
+        output_path = f"{isolate}.bygene.tsv"
+        
         with open(output_path, "w") as out:
-            print(f"Sample: {isolate}",file=out,flush=True)
+             header_written=False
+            
             for line in genecoords:
                 gene=line[0]
                 region=line[1]
-                print(f"Gene: {gene}",file=out,flush=True)
-                cmd = ["samtools","coverage","-A","-w","32","-r",region,bam_file]
-                subprocess.call(cmd, stdout=out, stderr=out)
+                #print(f"Gene: {gene}",file=out,flush=True)
+                cmd = ["samtools","coverage","-r",region,bam_file]
+                result = subprocess.call(cmd, capture_output=True,text=True,check=True)
+                
+                for line in result.stdout.strip().split('\n'):
+                    if line.startwith("rname"): 
+                        if not header_written:
+                            out.write('Gene\t'+line+'\n')
+                            header_written=True
+                        else:
+                            out.write(f"{gene}\t{line}\n")
+
